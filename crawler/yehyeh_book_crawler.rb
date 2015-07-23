75b9a5828e62253aaed934f8886bd631aaef6243
Yukai Huang <yukaihuang1993@hotmail.com>
2015-07-23 19:34:21 +0800

    heroku deployable

--- crawler/yehyeh_book_crawler.rb
+++ crawler/yehyeh_book_crawler.rb
@@ -5,12 +5,17 @@ require 'pry'
 require 'thread'
 require 'thwait'
 
+require 'book_toolkit'
+
 require 'iconv'
 
 class YehyehBookCrawler
   include CrawlerRocks::DSL
 
-  def initialize
+  def initialize update_progress: nil, after_each: nil
+    @update_progress_proc = update_progress
+    @after_each_proc = after_each
+
     @index_url = "http://www.yehyeh.com.tw/books.aspx"
     @ic = Iconv.new("utf-8//translit//IGNORE","utf-8")
     # ?a=000247&pgenow=0&sysmainid=books&titleid=&edtitleid=?&mode=dblist&edpagenow=94
@@ -44,28 +49,42 @@ class YehyehBookCrawler
           isbn = rows[1].css('td')[1].text.gsub(/\u{a0}$/, '')
           internal_code = rows[2].css('td')[1].text.gsub(/\u{a0}$/, '')
 
-          price = rows[3].css('td')[1].text.gsub(/[^\d]/, '').to_i
-          price = nil if price == 0
+          original_price = rows[3].css('td')[1].text.gsub(/[^\d]/, '').to_i
+          original_price = nil if original_price == 0
 
           external_image_url = grid.css('img').empty? ? nil : URI.join(@index_url, URI.encode(grid.css('img')[0][:src])).to_s
 
-          @books << {
+          invalid_isbn = nil
+          begin
+            isbn = BookToolkit.to_isbn13(isbn)
+          rescue Exception => e
+            invalid_isbn = isbn
+            isbn = nil
+          end
+
+          book = {
             name: name,
             url: url,
             author: author,
             isbn: isbn,
+            invalid_isbn: invalid_isbn,
             author: author,
             internal_code: internal_code,
-            price: price,
+            original_price: original_price,
             external_image_url: external_image_url,
+            known_supplier: 'yehyeh'
           }
+
+          @after_each_proc.call(book: book) if @after_each_proc
+
+          @books << book
         end
-        print "#{i} / #{page_num}\n"
+        # print "#{i} / #{page_num}\n"
       end # end thread
     end # end each page
     @books
   end
 end
 
-cc = YehyehBookCrawler.new
-File.write('yehyeh_books.json', JSON.pretty_generate(cc.books))
+# cc = YehyehBookCrawler.new
+# File.write('yehyeh_books.json', JSON.pretty_generate(cc.books))
